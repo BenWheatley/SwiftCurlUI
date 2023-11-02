190cfb8f7297c148601d5756d108f544042ce99b
Ben Wheatley <BenWheatley@users.noreply.github.com>
2023-11-02 12:58:53 +0100

    Default project

---
+++ SwiftCurlUI/SwiftCurlUI/SwiftCurlUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCurlUIApp.swift
+//  SwiftCurlUI
+//
+//  Created by Ben Wheatley on 02/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCurlUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
